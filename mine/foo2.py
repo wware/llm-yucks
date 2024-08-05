4c24efe2eee6844eadc8e72641966e4f1ff181b7
Will Ware <wware@veracode.com>
2024-08-05 16:27:29 -0400

    improved

--- mine/foo2.py
+++ mine/foo2.py
@@ -24,22 +24,12 @@ logger.info("Logger %s is set up", logger)
 PLACEHOLDER = object()
 
 class LLM:
-    def __init__(self, model="tinyllama"):
-        self.ollama_host = os.environ.get("OLLAMA_HOST")
+    def __init__(self, model):
+        self.ollama_host = os.environ.get(
+            "OLLAMA_HOST",
+             "http://localhost:11434"
+        )
         self.model = model
-        # curl http://localhost:11434/api/pull -d '{
-        # "name": "llama3"
-        # }'
-        response = self.post(url="/api/pull", model=model)
-        okay = False
-        for line in response.iter_lines():
-            if line:
-                # logger.info(line)
-                json_response = json.loads(line)
-                if json_response.get("status", "").lower() == "success":
-                    okay = True
-                    break
-        assert okay
 
     def post(self, url, **data):
         """Do a HTTP POST to the Ollama server
@@ -47,48 +37,38 @@ class LLM:
             Response: whatever is returned by the POST op
         """
         logger.info(self.ollama_host + url)
-        logger.info(data)
+        # logger.info(data)
         return requests.post(
             self.ollama_host + url,
             stream=True,
-            timeout=60,
+            timeout=20*60,
             json=data
         )
 
     def chat(self, _prompt=None, messages=None):
-        if _prompt is None:
-            _prompt = "Why is there air?"
-        if messages is None:
-            messages = [{
-                "role": "user",
-                "content": _prompt
-            }]
-        logger.info("")
-        message_groups = []
-        while messages:
-            logger.info(messages)
-            if PLACEHOLDER not in messages:
-                break
-            n = messages.index(PLACEHOLDER)
-            logger.info(n)
-            this, messages = messages[:n], messages[n+1:]
-            message_groups.append(this)
-        logger.info("")
-        if messages:
-            logger.info(messages)
-            message_groups.append(messages)
-        logger.info(message_groups)
-        for mgrp in message_groups:
-            logger.info(mgrp)
+        assert _prompt is None, "handle this case later"
+        assert isinstance(messages, list), messages
+
+        def run_thru(msgs):
             response = self.post(
                 url="/api/chat",
                 model=self.model,
-                messages=mgrp
+                # messages=mgrp
+                messages=msgs
             )
-            self.print_streamed_response(
+            answer = self.print_streamed_response(
                 response,
                 lambda response: response["message"]["content"]
             )
+            return answer
+
+        messages = messages[:]
+        while PLACEHOLDER in messages:
+            n = messages.index(PLACEHOLDER)
+            answer = run_thru(messages[:n])
+            messages[n] = {"role": "assistant",
+                           "content": answer}
+        run_thru(messages)
 
     def print_streamed_response(self, response, selector):
         assert callable(selector), selector
@@ -96,15 +76,15 @@ class LLM:
         for line in response.iter_lines():
             if line:
                 json_response = json.loads(line)
-                try:
-                    full_response += selector(json_response)
-                except:
+                if "error" in json_response:
                     logger.warning(line)
                     logger.warning(json_response)
-                    raise
-                if json_response['done']:
                     break
+                if json_response.get('done', False):
+                    break
+                full_response += selector(json_response)
         logger.info(full_response)
+        return full_response
 
     def generate(self, _prompt):
         response = self.post(
@@ -119,56 +99,59 @@ class LLM:
 
 
 # model_name = "tinyllama"
-model_name = "mistral"
-# model_name = "llama3"
+# model_name = "mistral"
+model_name = "llama3"
 # model_name = "codellama"
 llm = LLM(model_name)
-# prompt = "Why is the sky blue?"
-# prompt = "Why are manhole covers circular?"
-# prompt = "Is the space shuttle still flying?"
 
-# prompt = """\
-# Question about Flask-SQLAlchemy. There is a method `flask_sqlalchemy.SQLAlchemy.init_app`
-# where credentials are established for using a database. I have a database where a token
-# is periodically updated for security reasons. I find I cannot make repeated calls
-# to the `init_app` method, it should be called only once. What can I do instead?
-# """
+
+def code_snippet(pathname, start, finish):
+    with open(pathname, encoding='utf-8') as f:
+        lines = f.readlines()
+        return "\n".join(lines[start-1:finish])
+
+
+Z = """
+def is_truthy(s: str):
+    try:
+        return int(s) != 0
+    except ValueError:
+        return s.lower() in ("true", "yes", "1")
+
+
+def fetch_env_var(key, default=""):
+    key = key.upper().replace(" ", "_").replace("-", "_")
+    return os.environ.get(key, default)
+
+
+def set_env_var(key, value):
+    key = key.upper().replace(" ", "_").replace("-", "_")
+    os.environ[key] = str(value)
+
+
+def boolean_env_var(key):
+    return is_truthy(fetch_env_var(key, ""))
+"""
 
 messages = [
     {
         "role": "system",
         "content": """
-        If a prompt begins with "TRANSLATE", Convert the rest of the prompt from English to
-        RDF Turtle language. Nouns become entities or graph vertices.
-        If the prompt does not begin with "TRANSLATE" then take it as instructions and do
-        what it says.
+        If a prompt is Python code, then write google-style docstrings
+        for all classes, methods and functions.
+        Otherwise, use all python code encountered so far as context to answer any
+        questions.
         """
     },
     {
         "role": "user",
-        "content": "TRANSLATE Bob likes grilled cheese sandwiches."
-    },
-    {
-        "role": "assistant",
-        "content": """
-        @prefix :       <> .
-        @prefix owl:    <http://www.w3.org/2002/07/owl#> .
-        @prefix rdfs:   <http://www.w3.org/2000/01/rdf-schema#> .
-        @foaf:          <http://xmlns.com/foaf/0.1/> .
-        :GrilledCheeseSandwich rdfs:subClassOf :Food .
-        :Bob a :Person .
-        :Bob foaf:likes :GrilledCheeseSandwich .
-        """
-    },
-    {
-        "role": "user",
-        "content": "TRANSLATE Jenny likes seafood."
+        "content": Z
     },
     PLACEHOLDER,
     {
         "role": "user",
-        "content": "Take the Turtle piece about Jenny and seafood and translate it to RDF/XML."
-    },
+        "content": "Please write some pytest cases for these functions."
+    }
 ]
 
 llm.chat(messages=messages)
